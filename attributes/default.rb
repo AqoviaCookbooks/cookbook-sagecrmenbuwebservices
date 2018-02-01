f67e35e56fa05f22ca27f5fb22b0a07912187240
Ravindrakumar Solanki <ravindrakumars@interxion.com>
2018-02-01 16:26:40 +0000

    Fixed syntax error and implemented syntax suggested best practices

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@
 
 default['sagecrmenbuwebservices']['filename'] = 'Enbu Webservices Framework 2.2'
 default['sagecrmenbuwebservices']['filename'] = 'exe'
-default['sagecrmenbuwebservices']['url'] = 'http://www.yourserver.com/' + node['sagecrmenbuwebservices']['filename'] + '.' + node['sagecrmenbuwebservices']['filename'] 
+default['sagecrmenbuwebservices']['url'] = 'http://www.yourserver.com/' + node['sagecrmenbuwebservices']['filename'] + '.' + node['sagecrmenbuwebservices']['filename']
 default['sagecrmenbuwebservices']['checksum'] = '49ca95bfb041a5501a27d82435d6dafb4ec37c7bf0a2c28d76d01cfcf3e43e66'
 default['sagecrmenbuwebservices']['home'] = "#{Chef::Config['file_cache_path']}/#{node['sagecrmenbuwebservices']['filename']}/#{node['sagecrmenbuwebservices']['checksum']}"
 default['sagecrmenbuwebservices']['path'] = 'C:\Program Files (x86)\Sage\CRM\CRM\WWWRoot\CustomPages\Enbu\Framework'
@@ -35,20 +35,20 @@ default['sagecrmenbuwebservices']['installaccount']['account'] = node['sagecrm']
 default['sagecrmenbuwebservices']['installaccount']['password'] = node['sagecrm']['installaccount']['password'] || node['sagecrmenbuwebservices']['service']['password']
 
 username = node['sagecrmenbuwebservices']['service']['account']
-domain = ""
+domain = ''
 
 if username.include? '\\'
-	domain = username.split('\\')[0]
-	username = username.split('\\')[1]
+  domain = username.split('\\')[0]
+  username = username.split('\\')[1]
 end
 
 if username.include? '@'
-	domain = username.split('@')[1]
-	username = username.split('@')[0]
+  domain = username.split('@')[1]
+  username = username.split('@')[0]
 end
 
-if domain == ""  || domain == "."
-	domain = node["hostname"]
+if domain == '' || domain == '.'
+  domain = node['hostname']
 end
 
 default['sagecrmenbuwebservices']['database']['database_name'] = node['sagecrm']['database']['database_name'] || 'CRM'
@@ -56,4 +56,3 @@ default['sagecrmenbuwebservices']['properties']['User'] = username
 default['sagecrmenbuwebservices']['properties']['Password'] = node['sagecrmenbuwebservices']['service']['password']
 default['sagecrmenbuwebservices']['database']['host'] = node['sagecrm']['database']['host'] || '127.0.0.1'
 default['sagecrmenbuwebservices']['usex3interfaces'] = true
-
--- recipes/instance.rb
+++ recipes/instance.rb
@@ -9,33 +9,33 @@
 
 include_recipe 'autoit'
 
-if node['sagecrmenbuwebservices']['service']['account'] == ""
-    raise "Please configure Sage CRM Enbu Webservices service_account attribute"
+if node['sagecrmenbuwebservices']['service']['account'] == ''
+  raise 'Please configure Sage CRM Enbu Webservices service_account attribute'
 end
 
-if node['sagecrmenbuwebservices']['service']['password'] == ""
-    raise "Please configure Sage CRM Enbu Webservices service_account_password attribute"
+if node['sagecrmenbuwebservices']['service']['password'] == ''
+  raise 'Please configure Sage CRM Enbu Webservices service_account_password attribute'
 end
 
-if node['sagecrmenbuwebservices']['service']['password'] == ""
-    raise "Please configure Sage CRM Enbu Webservices service_account_password attribute"
+if node['sagecrmenbuwebservices']['service']['password'] == ''
+  raise 'Please configure Sage CRM Enbu Webservices service_account_password attribute'
 end
 
 username = node['sagecrmenbuwebservices']['service']['account']
-domain = ""
+domain = ''
 
 if username.include? '\\'
-	domain = username.split('\\')[0]
-	username = username.split('\\')[1]
+  domain = username.split('\\')[0]
+  username = username.split('\\')[1]
 end
 
 if username.include? '@'
-	domain = username.split('@')[1]
-	username = username.split('@')[0]
+  domain = username.split('@')[1]
+  username = username.split('@')[0]
 end
 
-if domain == ""  || domain == "."
-	domain = node["hostname"]
+if domain == '' || domain == '.'
+  domain = node['hostname']
 end
 
 ::Chef::Recipe.send(:include, Windows::Helper)
@@ -52,8 +52,8 @@ sagecrm_enbu_webservices_install_exe_path = File.join(working_directory, 'EnbuWe
 win_friendly_sagecrm_enbu_webservices_install_script_path = win_friendly_path(sagecrm_enbu_webservices_install_script_path)
 win_friendly_sagecrm_enbu_webservices_install_exe_path = win_friendly_path(sagecrm_enbu_webservices_install_exe_path)
 
-#Poor mans idempotency
-sagecrm_enbu_webservices_installed = ::File.directory?("#{node['sagecrmenbuwebservices']['application']['enbuwebservices']['physical_path']}")
+# Poor mans idempotency
+sagecrm_enbu_webservices_installed = ::File.directory?(node['sagecrmenbuwebservices']['application']['enbuwebservices']['physical_path'])
 filename = File.basename(node['sagecrmenbuwebservices']['url']).downcase
 
 installation_directory = File.join(working_directory, node['sagecrmenbuwebservices']['checksum'])
@@ -64,26 +64,26 @@ download_path = File.join(win_friendly_installation_directory, filename)
 template sagecrm_enbu_webservices_install_script_path do
   source 'EnbuWebServicesInstall.au3.erb'
   variables(
-    WorkingDirectory: win_friendly_installation_directory
+    WorkingDirectory: win_friendly_installation_directory,
     SetupFileName: filename
   )
-  not_if {sagecrm_enbu_webservices_installed}
+  not_if { sagecrm_enbu_webservices_installed }
 end
 
 execute "Check syntax #{win_friendly_sagecrm_enbu_webservices_install_script_path} with AutoIt" do
   command "\"#{File.join(node['autoit']['home'], '/Au3Check.exe')}\" \"#{win_friendly_sagecrm_enbu_webservices_install_script_path}\""
-  not_if {sagecrm_enbu_webservices_installed}
+  not_if { sagecrm_enbu_webservices_installed }
 end
 
 execute "Compile #{win_friendly_sagecrm_enbu_webservices_install_script_path} with AutoIt" do
   command "\"#{File.join(node['autoit']['home'], '/Aut2Exe/Aut2exe.exe')}\" /in \"#{win_friendly_sagecrm_enbu_webservices_install_script_path}\" /out \"#{win_friendly_sagecrm_enbu_webservices_install_exe_path}\" "
-  not_if {sagecrm_enbu_webservices_installed}
+  not_if { sagecrm_enbu_webservices_installed }
 end
 
 remote_file download_path do
   source node['sagecrmenbuwebservices']['url']
   checksum node['sagecrmenbuwebservices']['checksum']
-  not_if {sagecrm_enbu_webservices_installed}
+  not_if { sagecrm_enbu_webservices_installed }
 end
 
 win_friendly_psexec_path = win_friendly_path(File.join(node['pstools']['home'], 'psexec.exe'))
@@ -91,7 +91,7 @@ win_friendly_rdpplus_path = win_friendly_path(File.join(node['rdpplus']['home'],
 win_friendly_powershell_helper_path = win_friendly_path(File.join(node['autoit']['home'], 'Invoke-InDesktopSession.ps1'))
 
 powershell_script 'Install-SageCrmEnbuWebServices' do
-    code <<-EOH1
+  code <<-EOH1
 . "#{win_friendly_powershell_helper_path}"
 
 $username = '#{node['sagecrmenbuwebservices']['installaccount']['account']}'
@@ -100,7 +100,7 @@ $command = '#{win_friendly_sagecrm_enbu_webservices_install_exe_path}'
 $psexecPath = '#{win_friendly_psexec_path}'
 $rdpplusPath = '#{win_friendly_rdpplus_path}'
 
-$ErrorActionPreference = "Stop"  
+$ErrorActionPreference = "Stop"
 
 #We are unable to run the installer in a way that will allow it to start sage crm services in interactive mode. If the services exist that means the install is complete.
 $result = Invoke-InDesktopSession -username $username -password $password -command $command -psexecPath $psexecPath -rdpplusPath $rdpplusPath
@@ -110,10 +110,10 @@ if ($result.StandardOutput){
 }
 if ($result.ErrorOutput){
   Write-Error $result.ErrorOutput
-}	
+}
 exit $result.ExitCode
 
 EOH1
-    action :run
-    not_if {sagecrm_enbu_webservices_installed}
+  action :run
+  not_if { sagecrm_enbu_webservices_installed }
 end
